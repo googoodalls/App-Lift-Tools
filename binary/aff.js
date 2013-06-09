2d293d57ee7832fc92018b1ba162d131727d9363
StevenChen <googoodalls@gmail.com>
2013-06-09 22:59:07 +0800

    Add Binary Files

---
+++ binary/aff.js
@@ -0,0 +1,306 @@
+Load result: Start-end tags mismatch
+First node value: [], node child value: []
+First node value: [rsp], node child value: []
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+CandyCrushSaga_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+DragonCityMobile_small.jpg
+HayDay_small.jpg
+HayDay_small.jpg
+HayDay_small.jpg
+TheHobbitKingdoms_small.jpg
+TheHobbitKingdoms_small.jpg
+TheHobbitKingdoms_small.jpg
+TheHobbitKingdoms_small.jpg
+TheHobbitKingdoms_small.jpg
+TheHobbitKingdoms_small.jpg
+TheHobbitKingdoms_small.jpg
+PetRescueSaga_small.jpg
+PetRescueSaga_small.jpg
+PetRescueSaga_small.jpg
+PetRescueSaga_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+EmpireFourKingdoms_small.jpg
+BackyardMonstersUnleashed_small.jpg
+WilliamHillCasinoRealMoney_small.jpg
+BackyardMonstersUnleashed_small.jpg
+KingdomsofCamelot_small.jpg
+MonsterParadise_small.jpg
+KnightsampDragons_small.jpg
+KnightsampDragons_small.jpg
+BingoLane_small.jpg
+DragonEternityHD_small.jpg
+Spellstorm_small.jpg
+KingdomsofCamelot_small.jpg
+BingoLane_small.jpg
+BingoLane_small.jpg
+KingdomsofCamelot_small.jpg
+ClashofClans_small.jpg
+GSNCasino_small.jpg
+KingsEmpireDeluxe_small.jpg
+BattleBox_small.jpg
+KingsEmpireDeluxe_small.jpg
+BattleNations_small.jpg
+BloodBrothers_small.jpg
+GlobalThreatDeluxe_small.jpg
+NFLMatchups_small.jpg
+TransformersLegends_small.jpg
+DragonSkies_small.jpg
+ClashofClans_small.jpg
+NovaRaider_small.jpg
+KingdomsofCamelot_small.jpg
+MotorWorldCarFactory_small.jpg
+GSNCasino_small.jpg
+ShakeSpears_small.jpg
+DarklordLegends_small.jpg
+BloodBrothers_small.jpg
+CelticTribes_small.jpg
+FishwithAttitude_small.jpg
+CrazyTribes_small.jpg
+TransformersLegends_small.jpg
+FishwithAttitude_small.jpg
+RobinsonsIsland_small.jpg
+LordsampKnights_small.jpg
+BattleBox_small.jpg
+BloodBrothers_small.jpg
+DOTDefenderofTexel_small.jpg
+MermaidWorld_small.jpg
+MermaidWorld_small.jpg
+BigWinMLB_small.jpg
+SpartanWarsEliteEdition_small.jpg
+SpartanWarsEliteEdition_small.jpg
+VickytheVikingPart_small.jpg
+MotorWorldCarFactory_small.jpg
+KingdomVale_small.jpg
+LordsampKnights_small.jpg
+HellFire_small.jpg
+ClashofClans_small.jpg
+CelticTribes_small.jpg
+Solitaire_small.jpg
+HellFire_small.jpg
+Wackadoo_small.jpg
+DOTDefenderofTexel_small.jpg
+FairwaySolitaireHD_small.jpg
+HellFire_small.jpg
+DOTDefenderofTexel_small.jpg
+MyDiamonds_small.jpg
+HellFire_small.jpg
+DOTDefenderofTexel_small.jpg
+GardenscapesMansionMakeoverHD_small.jpg
+["1404", "CandyCrushSaga_small.jpg", "iPad", "JP"],
+["1406", "CandyCrushSaga_small.jpg", "iPhone", "JP"],
+["776", "CandyCrushSaga_small.jpg", "iPad", "AU"],
+["682", "CandyCrushSaga_small.jpg", "iPhone", "AU"],
+["1570", "CandyCrushSaga_small.jpg", "iPad", "NO"],
+["1084", "CandyCrushSaga_small.jpg", "iPad", "CA"],
+["1096", "CandyCrushSaga_small.jpg", "iPad", "US"],
+["1400", "CandyCrushSaga_small.jpg", "iPad", "BR"],
+["778", "CandyCrushSaga_small.jpg", "iPad", "CH"],
+["770", "CandyCrushSaga_small.jpg", "iPhone", "US"],
+["1528", "CandyCrushSaga_small.jpg", "iPhone", "NO"],
+["754", "CandyCrushSaga_small.jpg", "iPhone", "UK"],
+["1104", "CandyCrushSaga_small.jpg", "iPhone", "CH"],
+["1088", "CandyCrushSaga_small.jpg", "iPad", "UK"],
+["790", "CandyCrushSaga_small.jpg", "iPad", "FR"],
+["1094", "CandyCrushSaga_small.jpg", "iPhone", "NZ"],
+["688", "CandyCrushSaga_small.jpg", "iPhone", "CA"],
+["794", "CandyCrushSaga_small.jpg", "iPad", "IT"],
+["782", "CandyCrushSaga_small.jpg", "iPad", "DE"],
+["788", "CandyCrushSaga_small.jpg", "iPad", "FI"],
+["1102", "CandyCrushSaga_small.jpg", "iPad", "MX"],
+["796", "CandyCrushSaga_small.jpg", "iPad", "NZ"],
+["784", "CandyCrushSaga_small.jpg", "iPad", "DK"],
+["684", "CandyCrushSaga_small.jpg", "iPhone", "BR"],
+["740", "CandyCrushSaga_small.jpg", "iPhone", "DK"],
+["758", "CandyCrushSaga_small.jpg", "iPhone", "IE"],
+["748", "CandyCrushSaga_small.jpg", "iPhone", "FR"],
+["762", "CandyCrushSaga_small.jpg", "iPhone", "IT"],
+["780", "CandyCrushSaga_small.jpg", "iPad", "BE"],
+["738", "CandyCrushSaga_small.jpg", "iPhone", "DE"],
+["1092", "CandyCrushSaga_small.jpg", "iPad", "NL"],
+["1100", "CandyCrushSaga_small.jpg", "iPhone", "MX"],
+["798", "CandyCrushSaga_small.jpg", "iPad", "SE"],
+["686", "CandyCrushSaga_small.jpg", "iPhone", "BE"],
+["768", "CandyCrushSaga_small.jpg", "iPhone", "SE"],
+["764", "CandyCrushSaga_small.jpg", "iPhone", "NL"],
+["1366", "CandyCrushSaga_small.jpg", "iPad", "IE"],
+["786", "CandyCrushSaga_small.jpg", "iPad", "ES"],
+["800", "CandyCrushSaga_small.jpg", "iPad", "ZA"],
+["772", "CandyCrushSaga_small.jpg", "iPhone", "ZA"],
+["792", "CandyCrushSaga_small.jpg", "iPad", "AR"],
+["744", "CandyCrushSaga_small.jpg", "iPhone", "ES"],
+["1108", "CandyCrushSaga_small.jpg", "iPhone", "PT"],
+["1112", "CandyCrushSaga_small.jpg", "iPhone", "FI"],
+["1106", "CandyCrushSaga_small.jpg", "iPhone", "AR"],
+["1402", "CandyCrushSaga_small.jpg", "iPad", "PT"],
+["2190", "DragonCityMobile_small.jpg", "iPad", "DE"],
+["2262", "DragonCityMobile_small.jpg", "iPad", "IT"],
+["2088", "DragonCityMobile_small.jpg", "iPad", "UK"],
+["1754", "DragonCityMobile_small.jpg", "iPad", "US"],
+["2210", "DragonCityMobile_small.jpg", "iPhone", "AU"],
+["2208", "DragonCityMobile_small.jpg", "iPhone", "CA"],
+["2188", "DragonCityMobile_small.jpg", "iPhone", "DE"],
+["1750", "DragonCityMobile_small.jpg", "iPhone", "FR"],
+["2264", "DragonCityMobile_small.jpg", "iPhone", "IT"],
+["2090", "DragonCityMobile_small.jpg", "iPhone", "UK"],
+["1746", "DragonCityMobile_small.jpg", "iPhone", "US"],
+["2212", "DragonCityMobile_small.jpg", "iPad", "AU"],
+["2206", "DragonCityMobile_small.jpg", "iPad", "CA"],
+["1748", "DragonCityMobile_small.jpg", "iPad", "FR"],
+["1852", "HayDay_small.jpg", "iOS", "CH, NO, AU, US, AT, FR, UK, CA, DE, DK, BE"],
+["1920", "HayDay_small.jpg", "iOS", "BR, PT, IT, ES, CO, AR, ZA, CL, MX, NL, EC, UY, PY, CR, PE, DM, SE, PR"],
+["1922", "HayDay_small.jpg", "iOS", "AF, AX, AL, DZ, AS, AD, AO, AI, AQ, AG, AM, AW, AP, AZ, BS, BH, BD, BB, BY, BZ, BJ, BM, BT, BO, BA, BW, BV, IO, BN, BG, BF, BI, CM, CV, KY, CF, TD, CX, CC, KM, CG, CD, CK, CI, HR, CU, CY, CZ, DJ, DO, EG, SV, GQ, ER, EE, ET, EU, FK, FO, FJ, FI, GF, PF, TF, GA, GM, GE, GH, GI, GR, GL, GD, GP, GU, GT, GG, GN, GW, GY, HT, HM, VA, HN, HK, HU, IS, IN, ID, INT, IR, IQ, IE, IM, IL, JM, JP, JE, JO, KZ, KE, KI, KP, KR, KW, KG, LA, LV, LB, LS, LR, LY, LI, LT, LU, MO, MK, MG, MW, MY, MV, ML, MT, MH, MQ, MR, MU, YT, FM, MD, MC, MN, ME, MA, MZ, MM, NA, NR, NP, AN, NC, NZ, NI, NE, NG, NU, NF, MP, OM, PK, PW, PS, PA, PG, PH, PN, PL, QA, RE, RO, RU, RW, BL, KN, LC, MF, PM, VC, MS, SM, ST, SA, SN, RS, SC, SL, SG, SK, SI, SB, SO, GS, LK, SH, SD, SR, SJ, SZ, SY, TW, TJ, TZ, TH, TL, TG, TK, TO, TT, TN, TR, TM, TC, TV, UG, UA, AE, UM, UZ, VU, VE, VN, VG, VI, WF, EH, YE, ZM, ZW"],
+["1062", "TheHobbitKingdoms_small.jpg", "iOS", "US, SE, CA, AU"],
+["1038", "TheHobbitKingdoms_small.jpg", "iOS", "UK, CH"],
+["1606", "TheHobbitKingdoms_small.jpg", "iOS", "ES, NO, NL, DK, FR, LU"],
+["1864", "TheHobbitKingdoms_small.jpg", "iOS", "BE"],
+["1866", "TheHobbitKingdoms_small.jpg", "iOS", "TR"],
+["1610", "TheHobbitKingdoms_small.jpg", "iOS", "RU"],
+["1608", "TheHobbitKingdoms_small.jpg", "iOS", "PL, DE"],
+["2130", "PetRescueSaga_small.jpg", "iPad", "CA"],
+["2126", "PetRescueSaga_small.jpg", "iPhone", "CA"],
+["2132", "PetRescueSaga_small.jpg", "iPad", "SE"],
+["2128", "PetRescueSaga_small.jpg", "iPhone", "SE"],
+["2274", "EmpireFourKingdoms_small.jpg", "iPad", "CA"],
+["1546", "EmpireFourKingdoms_small.jpg", "iPad", "DE"],
+["1548", "EmpireFourKingdoms_small.jpg", "iPad", "FR"],
+["1648", "EmpireFourKingdoms_small.jpg", "iPad", "US"],
+["1516", "EmpireFourKingdoms_small.jpg", "iPhone", "DE"],
+["1542", "EmpireFourKingdoms_small.jpg", "iPhone", "FR"],
+["1650", "EmpireFourKingdoms_small.jpg", "iPhone", "US"],
+["2276", "EmpireFourKingdoms_small.jpg", "iPhone", "CA"],
+["1544", "EmpireFourKingdoms_small.jpg", "iPhone", "PL"],
+["1550", "EmpireFourKingdoms_small.jpg", "iPad", "PL"],
+["2300", "BackyardMonstersUnleashed_small.jpg", "iPad", "NZ"],
+["1872", "WilliamHillCasinoRealMoney_small.jpg", "iOS", "UK"],
+["2288", "BackyardMonstersUnleashed_small.jpg", "iPhone", "NZ"],
+["62", "KingdomsofCamelot_small.jpg", "iOS", "UK, US, AU, CA"],
+["356", "MonsterParadise_small.jpg", "iOS", "FR"],
+["1534", "KnightsampDragons_small.jpg", "iPhone", "UK, US, AU, CA, CH, SE, SG, PL, NO, NL, LU, IT, IL, FR, FI, DK, BE, AE, DE"],
+["1990", "KnightsampDragons_small.jpg", "iPad", "UK, US, AU, CA, CH, SE, SG, PL, NO, NL, LU, IT, IL, FR, FI, DK, BE, AE, DE"],
+["2026", "BingoLane_small.jpg", "iPad", "US"],
+["1536", "DragonEternityHD_small.jpg", "iPad", "UK, US, RU"],
+["2066", "Spellstorm_small.jpg", "iOS", "US, CA, UK, AU, SG, NO, SE, FI"],
+["1040", "KingdomsofCamelot_small.jpg", "iOS", "DK, SE, FR, DE, LU"],
+["2062", "BingoLane_small.jpg", "iPad", "AU"],
+["2060", "BingoLane_small.jpg", "iPad", "CA"],
+["1592", "KingdomsofCamelot_small.jpg", "iOS", "ES, IT, NO, NL, CH"],
+["1854", "ClashofClans_small.jpg", "iOS", "CH, AT, FR, NO, DE, SG, UK, NZ, US, CA, AU, FI, IE"],
+["2198", "GSNCasino_small.jpg", "iPad", "US, CA, NZ, AU"],
+["1522", "KingsEmpireDeluxe_small.jpg", "iOS", "UK, US"],
+["1486", "BattleBox_small.jpg", "iPad", ""],
+["2218", "KingsEmpireDeluxe_small.jpg", "iOS", "CA, AU, DE, FR, RU, CH, IT, ES"],
+["1168", "BattleNations_small.jpg", "iOS", "DE, FR"],
+["1654", "BloodBrothers_small.jpg", "iOS", "NO, CH, DK, FR, SE, UK, AU, US"],
+["2124", "GlobalThreatDeluxe_small.jpg", "iOS", "AU, CA, FR, DE, IT, UK, US, RU"],
+["1578", "NFLMatchups_small.jpg", "iOS", "US, CA"],
+["2202", "TransformersLegends_small.jpg", "iOS", "US, CA, AU"],
+["1564", "DragonSkies_small.jpg", "iOS", "UK, US, AU, CA, NO, NZ, IE"],
+["1918", "ClashofClans_small.jpg", "iOS", "TR, ID, BR, AE, HK, KW, AR, IT, ZA, CL, NL, BE, DK, GR, ES, SE, PE, PT, PR, CO, MX, BO, VE, EC, UY, PY, CR, PA, DO"],
+["2214", "NovaRaider_small.jpg", "iPad", "FR"],
+["1590", "KingdomsofCamelot_small.jpg", "iOS", "BE"],
+["1618", "MotorWorldCarFactory_small.jpg", "iOS", "UK, US, CA, AU, SE, NO, DE, FR"],
+["2196", "GSNCasino_small.jpg", "iPhone", "US, CA, AU, NZ"],
+["1778", "ShakeSpears_small.jpg", "iPhone", "RU"],
+["1860", "DarklordLegends_small.jpg", "iOS", "UK, US, AU, CA"],
+["1662", "BloodBrothers_small.jpg", "iOS", "DE, NZ, SG, BE, IE, CA, RO, RU"],
+["1216", "CelticTribes_small.jpg", "iOS", "AT, CH, ES, FR, IT, JP, RU, SE, UK, US"],
+["1958", "FishwithAttitude_small.jpg", "iPad", "AU, CA, UK, US"],
+["1218", "CrazyTribes_small.jpg", "iOS", "CH, DE, FR, RU"],
+["2204", "TransformersLegends_small.jpg", "iOS", "UK, SE, FI, SG"],
+["1950", "FishwithAttitude_small.jpg", "iPhone", "UK, US, AU, CA"],
+["1292", "RobinsonsIsland_small.jpg", "iPhone", "US"],
+["1214", "LordsampKnights_small.jpg", "iOS", "AT, CH, DE, ES, FR, IT, JP, RU, SE, UK, US"],
+["1378", "BattleBox_small.jpg", "iPhone", ""],
+["1664", "BloodBrothers_small.jpg", "iOS", "ID, ZA, AE, MY, KW, BN, NL, IT, GR, MO, CY, PT, ES, HR, UA, HU, HK, TH, TR, FI, SA, BR, CO"],
+["1652", "DOTDefenderofTexel_small.jpg", "iOS", "NZ, MY, SG, TH, US, FR, AU, DE, NL, CA, SE"],
+["1960", "MermaidWorld_small.jpg", "iPad", "AU, CA, UK, US"],
+["1946", "MermaidWorld_small.jpg", "iPhone", "AU, CA, UK, US"],
+["1884", "BigWinMLB_small.jpg", "iPhone", "CA, US"],
+["1880", "SpartanWarsEliteEdition_small.jpg", "iOS", "UK, US"],
+["2184", "SpartanWarsEliteEdition_small.jpg", "iOS", "CA, AU, DE, FR, RU, CH, JP, TW, HK"],
+["334", "VickytheVikingPart_small.jpg", "iOS", "AT, BE, FR, DE, LU, NL, CH, UK"],
+["1622", "MotorWorldCarFactory_small.jpg", "iOS", "AE, LU, PH, IT, NZ, MX, SG"],
+["1576", "KingdomVale_small.jpg", "iOS", "CA, US"],
+["1302", "LordsampKnights_small.jpg", "iOS", "TR, NL"],
+["1790", "HellFire_small.jpg", "iOS", "NO, AU, FR, UK, US, CA, TH"],
+["1924", "ClashofClans_small.jpg", "iOS", "AF, AX, AL, DZ, AS, AD, AO, AI, AQ, AG, AM, AW, AP, AZ, BS, BH, BD, BB, BY, BZ, BJ, BM, BT, BA, BW, BV, IO, BN, BG, BF, BI, CM, CV, KY, CF, TD, CX, CC, KM, CG, CD, CK, CI, HR, CU, CY, CZ, DJ, DM, EG, SV, GQ, ER, EE, ET, EU, FK, FO, FJ, GF, PF, TF, GA, GM, GE, GH, GI, GL, GD, GP, GU, GT, GG, GN, GW, GY, HT, HM, VA, HN, HU, IS, IN, INT, IR, IQ, IM, IL, JM, JP, JE, JO, KZ, KE, KI, KP, KR, KG, LA, LV, LB, LS, LR, LY, LI, LT, LU, MO, MK, MG, MW, MY, MV, ML, MT, MH, MQ, MR, MU, YT, FM, MD, MC, MN, ME, MA, MZ, MM, NA, NR, NP, AN, NC, NI, NE, NG, NU, NF, MP, OM, PK, PW, PS, PG, PH, PN, PL, QA, RE, RO, RU, RW, BL, KN, LC, MF, PM, VC, MS, SM, ST, SA, SN, RS, SC, SL, SK, SI, SB, SO, GS, LK, SH, SD, SR, SJ, SZ, SY, TW, TJ, TZ, TH, TL, TG, TK, TO, TT, TN, TM, TC, TV, UG, UA, UM, UZ, VU, VN, VG, VI, WF, EH, YE, ZM, ZW"],
+["1728", "CelticTribes_small.jpg", "iOS", "TR, NL"],
+["2248", "Solitaire_small.jpg", "iPhone", "US"],
+["1794", "HellFire_small.jpg", "iOS", "CH, DE, DK, MY, NL, PR, KW"],
+["2304", "Wackadoo_small.jpg", "iOS", "DE"],
+["1666", "DOTDefenderofTexel_small.jpg", "iOS", "UK, IT"],
+["1772", "FairwaySolitaireHD_small.jpg", "iPad", "FR, DE, CH, IT, NL, UK, US, AU, CA"],
+["1796", "HellFire_small.jpg", "iOS", "ID, AR, IL, IT, RU, BE, SG, AT, SE, AE, GR"],
+["1668", "DOTDefenderofTexel_small.jpg", "iOS", "MX, BR, RU, ES, ID"],
+["1322", "MyDiamonds_small.jpg", "iPhone", "US, CA, DE, AT, CH"],
+["1888", "HellFire_small.jpg", "iOS", "TR, ES, PH, MX, SA"],
+["1914", "DOTDefenderofTexel_small.jpg", "iOS", "DK, BE, PH"],
+["2068", "GardenscapesMansionMakeoverHD_small.jpg", "iPad", "US, CA, AU, NZ, UK, DE, FR, NL, IT, ES, CH, DK, SE, NO"],
+
+
+......finished....
